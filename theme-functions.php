2212959065b987835e594763604713b73a01596b
dorono <jazziseverywhere@gmail.com>
2022-07-26 06:34:42 -0500

    updated theme functions to allow admin bar to show up again

--- theme-functions.php
+++ theme-functions.php
@@ -826,4 +826,13 @@ if( ! function_exists('themify_theme_comment') ) {
 		<?php
 	}
 }
+
+function admin_bar(){
+
+	if(is_user_logged_in()){
+	  add_filter( 'show_admin_bar', '__return_true' , 1000 );
+	}
+  }
+  add_action('init', 'admin_bar' );
+
 ?>
