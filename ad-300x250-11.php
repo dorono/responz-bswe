5a8b38cc50eddaf700f2697fb14586b7d041c905
dorono <jazziseverywhere@gmail.com>
2023-09-02 22:25:34 -0500

    replaced wood and winds ad with Amazon affiliate links

--- ad-300x250-11.php
+++ ad-300x250-11.php
@@ -1,65 +1,16 @@
 <?php
 
-$Img1 = get_bloginfo('url') . "/wp-content/uploads/wood-and-winds-free-sax-neck-plug.jpg";
-$Alt1 = "Wood and Winds";
-$Url1 = " http://woodandwinds.com/pages/bwse";
-$ad_name1 = "wood-and-winds-free-sax-neck-plug";
-$slot_name1 = "Wood and Winds Free Sax Neck Plug";
+$Img1 = get_bloginfo('url') . "/wp-content/uploads/Universal-Method-1.png";
+$Alt1 = "Universal Method";
+$Url1 = "https://amzn.to/3Gnbv04";
+$class1 = "universal-method-300x250";
+$slot_name1 = "Amazon Link - Universal Method";
 
-$Img2 = get_bloginfo('url') . "/wp-content/uploads/wood-and-winds-free-sax-neck-plug.jpg";
-$Alt2 = "Wood and Winds";
-$Url2 = " http://woodandwinds.com/pages/bwse";
-$ad_name2 = "wood-and-winds-free-sax-neck-plug";
-$slot_name2 = "Wood and Winds Free Sax Neck Plug";
-
-// $Img1 = get_bloginfo('url') . "/wp-content/uploads/wall-stand.jpg";
-// $Alt1 = "Wood and Winds";
-// $Url1 = "https://woodandwinds.com/en-us/products/the-wall-hanger-saxophone-wall-stand-gift-for-saxophonist-player-tenor-alto-soprano-sax";
-// $ad_name1 = "wood-and-winds-wall-stand";
-// $slot_name1 = "Wood and Winds Wall Stand";
-
-// $Img2 = get_bloginfo('url') . "/wp-content/uploads/thumbrest.jpg";
-// $Alt2 = "Wood and Winds";
-// $Url2 = "https://woodandwinds.com/en-us/products/wooden-thumbrest-for-saxophone";
-// $ad_name2 = "wood-and-winds-thumbrest";
-// $slot_name2 = "Wood and Winds Thumbrest";
-
-// $Img3 = get_bloginfo('url') . "/wp-content/uploads/stand.jpg";
-// $Alt3 = "Wood and Winds";
-// $Url3 = "https://woodandwinds.com/en-us/products/solid-wood-sax-stand-available-immediately";
-// $ad_name3 = "wood-and-winds-stand";
-// $slot_name3 = "Wood and Winds Stand";
-
-// $Img4 = get_bloginfo('url') . "/wp-content/uploads/pearls.jpg";
-// $Alt4 = "Wood and Winds";
-// $Url4 = "https://woodandwinds.com/en-us/products/wooden-finger-buttons-for-saxophone";
-// $ad_name4 = "wood-and-winds-pearls";
-// $slot_name4 = "Wood and Winds Pearls";
-
-// $Img5 = get_bloginfo('url') . "/wp-content/uploads/neck-plug.jpg";
-// $Alt5 = "Wood and Winds";
-// $Url5 = "https://woodandwinds.com/en-us/products/mouthpiece-stand-for-sax-clarinet-etc";
-// $ad_name5 = "wood-and-winds-mouthpiece-stand";
-// $slot_name5 = "Wood and Winds Mouthpiece Stand";
-
-// $Img6 = get_bloginfo('url') . "/wp-content/uploads/neck-plug.jpg";
-// $Alt6 = "Wood and Winds";
-// $Url6 = "https://woodandwinds.com/en-us/products/exotic-wood-neck-plug-for-saxophone";
-// $ad_name6 = "wood-and-winds-neck-plug";
-// $slot_name6 = "Wood and Winds Neck Plug";
-
-// $Img1 = get_bloginfo('url') . "/wp-content/uploads/wood-and-winds-15-percent-off.jpg";
-// $Alt1 = "Wood and Winds 15% BSWE Discount";
-// $Url1 = "https://woodandwinds.com";
-// $ad_name1 = "wood-and-winds-15-percent-bswe-discount";
-// $slot_name1 = "Wood and Winds BSWE Discount";
-
-// $Img2 = get_bloginfo('url') . "/wp-content/uploads/wood-and-winds-15-percent-off.jpg";
-// $Alt2 = "Wood and Winds 15% BSWE Discount";
-// $Url2 = "https://woodandwinds.com";
-// $ad_name2 = "wood-and-winds-15-percent-bswe-discount";
-// $slot_name2 = "Wood and Winds BSWE Discount";
-
+$Img2 = get_bloginfo('url') . "/wp-content/uploads/25-daily-studies-300x250-1.jpg";
+$Alt2 = "25 Daily Studies";
+$Url2 = "https://amzn.to/45NcVLY";
+$class2 = "25-daily-studies-300x250";
+$slot_name2 = "Amazon Link - 25 Daily Studies";
 
 $num = mt_rand(1,2);
 
